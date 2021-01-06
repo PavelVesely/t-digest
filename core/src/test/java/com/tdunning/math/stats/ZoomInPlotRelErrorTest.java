bbe3e534e250b85393e2215ecb116efb8e032a89
Pavel Vesely <vesely@iuuk.mff.cuni.cz>
2021-01-06 13:48:57 +0100

    zoom in generator w/o BigDecimal and careful attack

--- core/src/test/java/com/tdunning/math/stats/ZoomInPlotRelErrorTest.java
+++ core/src/test/java/com/tdunning/math/stats/ZoomInPlotRelErrorTest.java
@@ -29,6 +29,8 @@ import org.junit.*;
 import java.io.*;
 import java.lang.Math;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -43,30 +45,37 @@ import org.junit.Ignore;
 public class ZoomInPlotRelErrorTest extends AbstractTest {
 
     private static final int[] CompressionsForTesting = {
-        100}; //100, 200, 500, 1000, 5000, 10000}; // param delta of t-digest
+        500}; //100, 200, 500, 1000, 5000, 10000}; // param delta of t-digest
     private static final int NumberOfPoints = 200; // number of points where we probe the rank estimates
 
     // params for generating the input
-    private static final int N = 50000; // 
-    private static final int K = 200; // N / K should be roughly at most 200 (otherwise, we don't have enough precision)
+    private static final int N = 200000; // 
+    private static final int K = 1000; // N / K should be roughly at most 200 (otherwise, we don't have enough precision)
     private static final int PrefixSize = 0; // number of points below zero in each iteration
-    private static final int NumberOfRepeats = 100; // N * NumberOfRepeats is (approx.) the number of points in the final instance
-    private static final String OutputFileName = "t-digest-genInput";
-    private static final String OutputFileDir = "/tmp/"; // CHANGE AS APPROPRIATE
-    private static final String StatsFileDir = "/tmp/"; // CHANGE AS APPROPRIATE
-
+    private static final int NumberOfRepeats = 150; // N * NumberOfRepeats is (approx.) the number of points in the final instance
+    private static final String InputStreamFileName = "t-digest-genInput";
+    private static final String InputStreamFileDir = "/aux/vesely/TD-inputs/"; // CHANGE AS APPROPRIATE
+    private static final String DigestStatsFileName = "t-digest-results";
+    private static final String DigestStatsDir = "../../../../TD-stats/"; // CHANGE AS APPROPRIATE
+    private static final String FileSuffix = ".csv";
+    //private static final String StatsFileDir = "/home/vesely/research/biasedQuantiles/TD-stats/"; // CHANGE AS APPROPRIATE
+    
     @BeforeClass
     public static void freezeSeed() {
         RandomUtils.useTestSeed();
     }
 
-    @Test
+    @Test //@Ignore
     public void testZoomIn() throws FileNotFoundException, IOException {
 
         List<Double> sortedData = new ArrayList<Double>();
         List<Double> sortedDataPart = new ArrayList<Double>();
         List<Double> data = new ArrayList<Double>();
-        PrintWriter w = new PrintWriter(OutputFileDir + OutputFileName);
+        Files.createDirectories(Paths.get(InputStreamFileDir));
+        String inputFilePath = InputStreamFileDir + InputStreamFileName + "_K=" + String.valueOf(K) + "_N=" + String
+                    .valueOf(N) + "_PS=" + String.valueOf(PrefixSize) + "_repeats=" + String
+                    .valueOf(NumberOfRepeats) + FileSuffix;
+        PrintWriter w = new PrintWriter(inputFilePath);
         int n = 0;
         for (int r = 0; r < NumberOfRepeats; r++) {  // same instance is essentially repeated
             double max = Double.MAX_VALUE / (100 * K + r); // initial interval max
@@ -109,70 +118,81 @@ public class ZoomInPlotRelErrorTest extends AbstractTest {
         }
         Collections.sort(sortedData);
         w.close();
-
+        System.out.println("file with generated input: " + inputFilePath);
+        System.out.flush();
+        
         for (int compr : CompressionsForTesting) {
             TDigest digest = new MergingDigest(compr);
             digest.setScaleFunction(ScaleFunction.K_2); //_GLUED;
-            String outName =
-                StatsFileDir + OutputFileName + "_K=" + String.valueOf(K) + "_N=" + String
-                    .valueOf(N) + "_PS=" + String.valueOf(PrefixSize) + "_repeats=" + String
-                    .valueOf(NumberOfRepeats) + "-stats-PP_" + String.valueOf(NumberOfPoints)
-                    + "_compr_" + String.valueOf(compr) + "_" + digest.scale.toString();
-            System.out.printf("stats file:" + outName + "\n");
-            File fout = new File(outName);
-            fout.createNewFile();
-            FileWriter fwout = new FileWriter(fout);
             for (double item : data) {
                 digest.add(item);
             }
             digest.compress();
-            //System.out.printf("computing rel. errors\n");
-            //System.out.flush();
-
-            fwout.write("true quantile;true rank;est. rank;rel. error;abs. error;item\n");
-            for (int t = 0; t < NumberOfPoints; t++) {
-                //THE FOLLOWING IS EXTREMELY SLOW: Dist.cdf(item, sortedData);
-                int rTrue = (int) Math.ceil(t / (float) NumberOfPoints * n) + 1;
-                double item = sortedData.get(rTrue - 1);
-                // handling duplicate values -- rank is then rather an interval
-                int rTrueMin = rTrue;
-                int rTrueMax = rTrue;
-                while (rTrueMin >= 2 && item == sortedData.get(rTrueMin - 2)) {
-                    rTrueMin--;
-                }
-                while (rTrueMax < sortedData.size() && item == sortedData.get(rTrueMax)) {
-                    rTrueMax++;
-                }
-                double rEst = digest.cdf(item) * n + 0.5;
-                double relErr = 0;
-                double addErr = 0;
-                if (rEst < rTrueMin) {
-                    relErr = Math.abs(rTrueMin - rEst) / rTrue;
-                    addErr = (rEst - rTrueMin) / n;
-                }
-                if (rEst > rTrueMax) {
-                    relErr = Math.abs(rTrueMax - rEst) / rTrue;
-                    addErr = (rEst - rTrueMax) / n;
-                }
-                fwout.write(String
-                    .format("%.6f;%d;%.6f;%.6f;%.6f;%s\n", rTrue / (float) n, (int) rTrue, rEst,
-                        relErr, addErr, String.valueOf(item)));
-            }
-            fwout.write("\n");
-            fwout.write(String.format("n=%d\n", n));
-            fwout.write(String.format("scale func. = %s\n", digest.scale.toString()));
-            fwout.write(String.format("delta = %d (compression param of t-digest)\n", compr));
-            fwout.write(String.format("# of centroids = %d\n", digest.centroids().size()));
-            fwout.write(String.format("size in bytes = %d\n", digest.byteSize()));
-            fwout.write("\nCentroids:\n");
-            for (Centroid centr : digest.centroids()) {
-                fwout.write(centr.toString() + "\n");
-            }
-
-            fwout.close();
-
+            System.out.println("processing by t-digest done for compression =" + String.valueOf(compr));
             System.out.flush();
+
+            writeResults(compr, n, digest, sortedData, DigestStatsDir,
+                DigestStatsDir + DigestStatsFileName + "_K=" + String.valueOf(K) + "_N=" + String
+                    .valueOf(N) + "_PS=" + String.valueOf(PrefixSize) + "_repeats=" + String
+                    .valueOf(NumberOfRepeats) + "-stats-PP_" + String.valueOf(NumberOfPoints)
+                    + "_compr_" + String.valueOf(compr) + digest.scale.toString() + FileSuffix);
         }
+    }
+    
+    private static void writeResults(int compr, int size, TDigest digest,
+        List<Double> sortedData, String digestStatsDir, String outName) throws
+        IOException {
+        Files.createDirectories(Paths.get(DigestStatsDir));
+        System.out.printf("stats file:" + outName + "\n");
+        File fout = new File(outName);
+        fout.createNewFile();
+        FileWriter fwout = new FileWriter(fout);
+
+        //System.out.printf("computing rel. errors\n");
+        //System.out.flush();
+
+        fwout.write("true quantile;true rank;est. rank;rel. error;abs. error;item\n");
+        for (int t = 0; t < NumberOfPoints; t++) {
+            //THE FOLLOWING IS EXTREMELY SLOW: Dist.cdf(item, sortedData);
+            int rTrue = (int) Math.ceil(t / (float) NumberOfPoints * size) + 1;
+            double item = sortedData.get(rTrue - 1);
+            // handling duplicate values -- rank is then rather an interval
+            int rTrueMin = rTrue;
+            int rTrueMax = rTrue;
+            while (rTrueMin >= 2 && item == sortedData.get(rTrueMin - 2)) {
+                rTrueMin--;
+            }
+            while (rTrueMax < sortedData.size() && item == sortedData.get(rTrueMax)) {
+                rTrueMax++;
+            }
+            double rEst = digest.cdf(item) * size + 0.5;
+            double relErr = 0;
+            double addErr = 0;
+            if (rEst < rTrueMin) {
+                relErr = Math.abs(rTrueMin - rEst) / rTrue;
+                addErr = (rEst - rTrueMin) / size;
+            }
+            if (rEst > rTrueMax) {
+                relErr = Math.abs(rTrueMax - rEst) / rTrue;
+                addErr = (rEst - rTrueMax) / size;
+            }
+            fwout.write(String
+                .format("%.6f;%d;%.6f;%.6f;%.6f;%s\n", rTrue / (float) size, (int) rTrue, rEst,
+                    relErr, addErr, String.valueOf(item)));
+        }
+        fwout.write("\n");
+        fwout.write(String.format("n=%d\n", size));
+        fwout.write(String.format("scale func. = %s\n", digest.scale.toString()));
+        fwout.write(String.format("delta = %d (compression param of t-digest)\n", compr));
+        fwout.write(String.format("# of centroids = %d\n", digest.centroids().size()));
+        fwout.write(String.format("size in bytes = %d\n", digest.byteSize()));
+        fwout.write("\nCentroids:\n");
+        for (Centroid centr : digest.centroids()) {
+            fwout.write(centr.toString() + "\n");
+        }
+        fwout.close();
+        System.out.flush();
+
     }
 
 }
