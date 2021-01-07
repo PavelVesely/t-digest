b5860abaa5faa4a34aeab06b8a384e9330418b2f
Pavel Vesely <vesely@iuuk.mff.cuni.cz>
2021-01-07 16:46:15 +0100

    IID generator and "zoom in" generator with floats

--- core/src/test/java/com/tdunning/math/stats/ZoomInPlotRelErrorTest.java
+++ core/src/test/java/com/tdunning/math/stats/ZoomInPlotRelErrorTest.java
@@ -52,7 +52,7 @@ public class ZoomInPlotRelErrorTest extends AbstractTest {
     private static final int N = 200000; // 
     private static final int K = 1000; // N / K should be roughly at most 200 (otherwise, we don't have enough precision)
     private static final int PrefixSize = 0; // number of points below zero in each iteration
-    private static final int NumberOfRepeats = 150; // N * NumberOfRepeats is (approx.) the number of points in the final instance
+    private static final int NumberOfRepeats = 400; // N * NumberOfRepeats is (approx.) the number of points in the final instance
     private static final String InputStreamFileName = "t-digest-genInput";
     private static final String InputStreamFileDir = "/aux/vesely/TD-inputs/"; // CHANGE AS APPROPRIATE
     private static final String DigestStatsFileName = "t-digest-results";
@@ -65,7 +65,7 @@ public class ZoomInPlotRelErrorTest extends AbstractTest {
         RandomUtils.useTestSeed();
     }
 
-    @Test //@Ignore
+    @Test  
     public void testZoomIn() throws FileNotFoundException, IOException {
 
         List<Double> sortedData = new ArrayList<Double>();
@@ -139,6 +139,43 @@ public class ZoomInPlotRelErrorTest extends AbstractTest {
         }
     }
     
+    @Ignore  
+    public void testZoomInIIDgenerator() throws FileNotFoundException, IOException {
+        List<Double> sortedData = new ArrayList<Double>();
+        List<Double> data = new ArrayList<Double>();
+        Files.createDirectories(Paths.get(InputStreamFileDir));
+        String inputFilePath = InputStreamFileDir + InputStreamFileName + "_IIDitems" + "_N=" + String.valueOf(N) + FileSuffix;
+        PrintWriter w = new PrintWriter(inputFilePath);
+        Random rand = new Random();
+        int n;
+        final int maxExp = (int)Math.log10(Double.MAX_VALUE / 100);
+        for (n = 0; n < N; n++) {  // same instance is essentially repeated
+            double item = rand.nextDouble() * Math.pow(10, rand.nextInt(2*maxExp) - maxExp);
+            data.add(item);
+            sortedData.add(item);
+            w.println(String.valueOf(item));
+        }
+        Collections.sort(sortedData);
+        w.close();
+        System.out.println("file with generated input: " + inputFilePath);
+        System.out.flush();
+        
+        for (int compr : CompressionsForTesting) {
+            TDigest digest = new MergingDigest(compr);
+            digest.setScaleFunction(ScaleFunction.K_2); //_GLUED;
+            for (double item : data) {
+                digest.add(item);
+            }
+            digest.compress();
+            System.out.println("processing by t-digest done for compression =" + String.valueOf(compr));
+            System.out.flush();
+
+            writeResults(compr, n, digest, sortedData, DigestStatsDir,
+                DigestStatsDir + DigestStatsFileName + "_IIDitems" + "_N=" + String.valueOf(N) + "-stats-PP_" + String.valueOf(NumberOfPoints)
+                    + "_compr_" + String.valueOf(compr) + digest.scale.toString() + FileSuffix);
+        }
+    }
+    
     private static void writeResults(int compr, int size, TDigest digest,
         List<Double> sortedData, String digestStatsDir, String outName) throws
         IOException {
@@ -156,6 +193,136 @@ public class ZoomInPlotRelErrorTest extends AbstractTest {
             //THE FOLLOWING IS EXTREMELY SLOW: Dist.cdf(item, sortedData);
             int rTrue = (int) Math.ceil(t / (float) NumberOfPoints * size) + 1;
             double item = sortedData.get(rTrue - 1);
+            // handling duplicate values -- rank is then rather an interval
+            int rTrueMin = rTrue;
+            int rTrueMax = rTrue;
+            while (rTrueMin >= 2 && item == sortedData.get(rTrueMin - 2)) {
+                rTrueMin--;
+            }
+            while (rTrueMax < sortedData.size() && item == sortedData.get(rTrueMax)) {
+                rTrueMax++;
+            }
+            double rEst = digest.cdf(item) * size + 0.5;
+            double relErr = 0;
+            double addErr = 0;
+            if (rEst < rTrueMin) {
+                relErr = Math.abs(rTrueMin - rEst) / rTrue;
+                addErr = (rEst - rTrueMin) / size;
+            }
+            if (rEst > rTrueMax) {
+                relErr = Math.abs(rTrueMax - rEst) / rTrue;
+                addErr = (rEst - rTrueMax) / size;
+            }
+            fwout.write(String
+                .format("%.6f;%d;%.6f;%.6f;%.6f;%s\n", rTrue / (float) size, (int) rTrue, rEst,
+                    relErr, addErr, String.valueOf(item)));
+        }
+        fwout.write("\n");
+        fwout.write(String.format("n=%d\n", size));
+        fwout.write(String.format("scale func. = %s\n", digest.scale.toString()));
+        fwout.write(String.format("delta = %d (compression param of t-digest)\n", compr));
+        fwout.write(String.format("# of centroids = %d\n", digest.centroids().size()));
+        fwout.write(String.format("size in bytes = %d\n", digest.byteSize()));
+        fwout.write("\nCentroids:\n");
+        for (Centroid centr : digest.centroids()) {
+            fwout.write(centr.toString() + "\n");
+        }
+        fwout.close();
+        System.out.flush();
+
+    }
+    
+    @Ignore 
+    public void testZoomInFloat() throws FileNotFoundException, IOException {
+
+        List<Float> sortedData = new ArrayList<Float>();
+        List<Float> sortedDataPart = new ArrayList<Float>();
+        List<Float> data = new ArrayList<Float>();
+        Files.createDirectories(Paths.get(InputStreamFileDir));
+        String inputFilePath = InputStreamFileDir + InputStreamFileName + "_Float" + "_K=" + String.valueOf(K) + "_N=" + String
+                    .valueOf(N) + "_PS=" + String.valueOf(PrefixSize) + "_repeats=" + String
+                    .valueOf(NumberOfRepeats) + FileSuffix;
+        PrintWriter w = new PrintWriter(inputFilePath);
+        int n = 0;
+        for (int r = 0; r < NumberOfRepeats; r++) {  // same instance is essentially repeated
+            float max = Float.MAX_VALUE / (10 * K + r); // initial interval max
+            float min = 0; //Float.MIN_VALUE;
+            int nn = 0;
+            for (int i = -PrefixSize + 1; i <= K - PrefixSize;
+                i++) {//for (int i = 0; i <= K; i++) {
+                float item = min + (i / (float) (K + 1)) * (max - min);
+                data.add(item);
+                sortedData.add(item);
+                sortedDataPart.add(item);
+                w.println(String.valueOf(item));
+                nn++;
+                n++;
+            }
+            int maxREindex = -1;
+            while (nn < N) {
+                maxREindex = -Collections.binarySearch(sortedDataPart, Float.MIN_NORMAL) - 1;
+                min = 0; //maxREindex > 0 ? sortedDataPart.get(maxREindex - 1) : 0;
+                max = sortedDataPart.get(maxREindex);
+                //System.out.printf(String.format("phase maxRErTrue %d;\tmin %s;\tmax %s;\t means %d\n",
+                //		maxREindex, String.valueOf(min), String.valueOf(max), digest.centroidCount()));
+                if ((max - min) / (float) (K + 1) < Float.MIN_VALUE) {
+                    System.out.printf("TOO SMALL max - min\n");
+                }
+                for (int i = -PrefixSize; i <= K - PrefixSize; i++) {
+                    float item = min + (i / (float) (K + 1)) * (max - min);
+                    //if (item != 0) { // try to avoid duplicates
+                    data.add(item);
+                    sortedData.add(item);
+                    sortedDataPart.add(item);
+                    w.println(String.valueOf(item));
+                    n++;
+                    nn++;
+                    //}
+                }
+                Collections.sort(sortedDataPart);
+            }
+            sortedDataPart.clear();
+        }
+        Collections.sort(sortedData);
+        w.close();
+        System.out.println("file with generated input: " + inputFilePath);
+        System.out.flush();
+        
+        for (int compr : CompressionsForTesting) {
+            TDigest digest = new MergingDigest(compr);
+            digest.setScaleFunction(ScaleFunction.K_2); //_GLUED;
+            for (float item : data) {
+                digest.add(item);
+            }
+            digest.compress();
+            System.out.println("processing by t-digest done for compression =" + String.valueOf(compr));
+            System.out.flush();
+
+            writeResultsFloat(compr, n, digest, sortedData, DigestStatsDir,
+                DigestStatsDir + DigestStatsFileName + "_Float" + "_K=" + String.valueOf(K) + "_N=" + String
+                    .valueOf(N) + "_PS=" + String.valueOf(PrefixSize) + "_repeats=" + String
+                    .valueOf(NumberOfRepeats) + "-stats-PP_" + String.valueOf(NumberOfPoints)
+                    + "_compr_" + String.valueOf(compr) + digest.scale.toString() + FileSuffix);
+        }
+    }
+    
+    private static void writeResultsFloat(int compr, int size, TDigest digest,
+        List<Float> sortedData, String digestStatsDir, String outName) throws
+        IOException {
+        Files.createDirectories(Paths.get(DigestStatsDir));
+        System.out.printf("stats file:" + outName + "\n");
+        File fout = new File(outName);
+        fout.createNewFile();
+        FileWriter fwout = new FileWriter(fout);
+
+        //System.out.printf("computing rel. errors\n");
+        //System.out.flush();
+
+        fwout.write("true quantile;true rank;est. rank;rel. error;abs. error;item\n");
+        for (int t = 0; t < NumberOfPoints; t++) {
+            //THE FOLLOWING IS EXTREMELY SLOW: Dist.cdf(item, sortedData);
+            int rTrue = (int) Math.ceil(t / (float) NumberOfPoints * size) + 1;
+            float item = sortedData.get(rTrue - 1);
             // handling duplicate values -- rank is then rather an interval
             int rTrueMin = rTrue;
             int rTrueMax = rTrue;
